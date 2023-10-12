5f299a38476a23b62cf0b931dd9cae84c8062bde
Kaka123 <dhiratht25@rism.ac.th>
2023-10-12 12:29:05 +0700

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,20 @@
-import "../styles/globals.css";
+
+
+import '../styles/globals.css';
 import Navbar from "../components/Navbar";
 import Bottombar from "../components/Bottombar";
 import type { AppProps } from "next/app";
 import React from "react";
 import Head from "next/head";
 
+
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>Thailand-RIS - iGEM 2023</title>
-        <meta
-          name="description"
-          content="Designed by Miu. Programmed by Dhirath (Kaka) Thanglerdsumpan, Thamogyna Kodi, Settachai (Jacky) Lertwasana, Maera Singh,"
-        />
-        <link
-          rel="icon"
-          href="https://static.igem.wiki/teams/4629/wiki/favicon6.png"
-        />
+        <meta name="description" content="Designed by Miu. Programmed by Dhirath (Kaka) Thanglerdsumpan, Thamogyna Kodi, Settachai (Jacky) Lertwasana, Maera Singh," />
+        <link rel="icon" href="https://static.igem.wiki/teams/4629/wiki/favicon6.png" />
       </Head>
       <Navbar />
       <Component {...pageProps} />
@@ -25,3 +22,5 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     </>
   );
 }
+
+
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import {useRef} from 'react';
+import Link from 'next/link'
+
+function HomePage() {
+    return(
+        <div>
+            <div class="yBox100"></div>
+            <h1>HOME</h1>
+        </div>
+    );
+}
+export default HomePage;
